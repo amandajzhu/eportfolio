00a03c68d6e45818ba681dd7648a7da254888fb1
amandajzhu <amandazhu0905@gmail.com>
2025-04-19 18:40:02 -0400

    Add ePortfolio content

---
+++ index.js
@@ -0,0 +1,17 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <nav>
+        <a href="../index.html">Amanda Zhu - ePortfolio</a>
+    </nav>
+`;
+
+document.body.prepend(template.content);
+
+template.innerHTML = `
+    <div class="footer">
+        <p>Amanda Zhu, 2025-04-19</p>
+    </div>
+`;
+
+document.body.appendChild(template.content);
